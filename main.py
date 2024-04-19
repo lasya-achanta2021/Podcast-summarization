c978d1a5f8075db7e0d6b14eca8446e3ca3eadad
lasya-achanta2021 <94073629+lasya-achanta2021@users.noreply.github.com>
2024-04-19 20:22:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,49 +4,32 @@ import json
 from api_04 import save_transcript
 
 st.title("Podcast Summaries")
-
-json_files = glob.glob('*.json')
-
-episode_id = st.sidebar.text_input("Episode ID")
-button = st.sidebar.button("Download Episode summary")
-
+episode_id = st.sidebar.text_input('Please input an episode id')
+button = st.sidebar.button('Get podcast summary!', on_click=save_transcript, args=(episode_id, ))
 
 def get_clean_time(start_ms):
     seconds = int((start_ms / 1000) % 60)
     minutes = int((start_ms / (1000 * 60)) % 60)
-    hours = int((start_ms / (1000 * 60 * 60)) % 24)
+    hours = int((start_ms / (1000 * 60 *60)) % 24)
     if hours > 0:
         start_t = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
     else:
         start_t = f'{minutes:02d}:{seconds:02d}'
-    return start_t
-
+        
+    return start_t 
 
 if button:
-    success = save_transcript(episode_id)
-    if not success:
-        st.error("Error: Failed to save transcript. Please check the episode ID and try again.")
-    else:
-        st.success("Transcript saved successfully.")
-
-    filename = f"{episode_id}.json"  # Assume the transcript is saved as JSON
-    try:
-        with open(filename, 'r') as f:
-            data = json.load(f)
-
+    filename = episode_id + '_chapters.json'
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        
         chapters = data['chapters']
-        episode_title = data['episode_title']
-        thumbnail = data['thumbnail']
         podcast_title = data['podcast_title']
-        audio = data['audio_url']
-
-        st.header(f"{podcast_title} - {episode_title}")
-        st.image(thumbnail, width=200)
-        st.markdown(f'#### {episode_title}')
-
-        for chp in chapters:
-            with st.expander(chp['gist'] + ' - ' + get_clean_time(chp['start'])):
-                st.write(chp['summary'])
-
-    except FileNotFoundError:
-        st.error(f"Error: File '{filename}' not found.")
+        episode_title = data['episode_title']
+        thumbnail = data['episode_thumbnail']
+        
+    st.header(f'{podcast_title} - {episode_title}')
+    st.image(thumbnail)
+    for chp in chapters:
+        with st.expander(chp['gist'] + '-' + get_clean_time(chp[''])):
+            chp['summary']
